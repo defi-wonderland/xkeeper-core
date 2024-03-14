2b9c91f7a9fd289564da183cd7f443c2a975cb71
0xdeo <0xdeo@defi.sucks>
2024-03-14 16:02:35 -0400

    fix: ashi comments addressed

--- solidity/contracts/core/Keep3rSponsor.sol
+++ solidity/contracts/core/Keep3rSponsor.sol
@@ -12,11 +12,12 @@ import {EnumerableSet} from '@openzeppelin/utils/structs/EnumerableSet.sol';
 contract Keep3rSponsor is Ownable, IKeep3rSponsor {
   using EnumerableSet for EnumerableSet.AddressSet;
 
-  EnumerableSet.AddressSet private sponsoredJobs;
-  IAutomationVault constant VAULT = IAutomationVault(0x255ed36E805aEf7f6ad94e3647fA0aED15dBb14D);
-  IOpenRelay constant RELAY = IOpenRelay(0xE8E1b32340f527125721903a3947c937dc72e140);
+  EnumerableSet.AddressSet private _sponsoredJobs;
+  IOpenRelay immutable RELAY;
 
-  constructor() Ownable() {}
+  constructor(address relay) Ownable() {
+    RELAY = IOpenRelay(relay);
+  }
 
   modifier validateAndPayKeeper() {
     if (!_KEEP3R_V2.isKeeper(msg.sender)) revert KeeperNotValid();
@@ -25,36 +26,45 @@ contract Keep3rSponsor is Ownable, IKeep3rSponsor {
     _KEEP3R_V2.worked(msg.sender);
   }
 
-  modifier isSponsored(address job) {
-    if (!jobSponsored(job)) revert JobNotSponsored();
-    _;
-  }
-
   // @note unsure if jobs should also be added to automation vault or if assumed to already be in approvedJobs list
-  function addJobs(address[] calldata jobs) public onlyOwner {
-    for (uint256 i = 0; i < jobs.length; i++) {
-      sponsoredJobs.add(jobs[i]);
+  function addJobs(address[] calldata _jobs) public onlyOwner {
+    for (uint256 _i; _i < _jobs.length;) {
+      _sponsoredJobs.add(_jobs[_i]);
+
+      unchecked {
+        ++_i;
+      }
     }
   }
 
-  function removeJobs(address[] calldata jobs) public onlyOwner {
-    for (uint256 i = 0; i < jobs.length; i++) {
-      sponsoredJobs.remove(jobs[i]);
+  function removeJobs(address[] calldata _jobs) public onlyOwner {
+    for (uint256 _i; _i < _jobs.length;) {
+      _sponsoredJobs.remove(_jobs[_i]);
+
+      unchecked {
+        ++_i;
+      }
     }
   }
 
-  function jobSponsored(address job) public view returns (bool inSet) {
-    inSet = sponsoredJobs.contains(job);
-  }
-
-  function exec(IAutomationVault.ExecData[] calldata _execData) external validateAndPayKeeper {
+  function exec(IAutomationVault.ExecData[] calldata _execData, IAutomationVault _vault) external validateAndPayKeeper {
     // @note if _execData holds data for just 1 job, this is unnecessary
-    for (uint256 i = 0; i < _execData.length; i++) {
-      if (!jobSponsored(_execData[i].job)) revert JobNotSponsored();
-      // saves gas to emit here rather than making another for loop
-      emit JobExecuted(_execData[i].job);
+    for (uint256 _i; _i < _execData.length;) {
+      if (!_sponsoredJobs.contains(_execData[_i].job)) revert JobNotSponsored();
+
+      unchecked {
+        ++_i;
+      }
     }
 
-    RELAY.exec(VAULT, _execData, msg.sender);
+    RELAY.exec(_vault, _execData, msg.sender);
+
+    for (uint256 _i; _i < _execData.length;) {
+      emit JobExecuted(_execData[_i].job);
+
+      unchecked {
+        ++_i;
+      }
+    }
   }
 }
